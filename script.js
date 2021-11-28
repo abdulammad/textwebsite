62766eb76d557cacc6f0dc8e29ef538537145b5c
abdulammad <95169266+abdulammad@users.noreply.github.com>
2021-11-28 05:31:38 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function kane(){
+    alert("Login successfully");
+}  
+function kamo(){
+    alert("Bought successfully");
+}  
+
+function kano(){
+    alert("Register successfully");
+
+
+alert('Please Wait....');
+
+
+
+   
+
+
+
+
+  
+
+}
